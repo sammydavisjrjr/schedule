0bd41d04898bca21af21e7f455ebc951f0583ae9
Mark Russell <sammydavis_jrjr@hotmail.com>
2013-02-14 10:00:56 -0700

    Test file for static pages..

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+
+    it "should have the content 'Schedule IT'" do
+      visit '/static_pages/home'
+      page.should have_content('Schedule IT')
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                                :text => "Schedule IT | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                                :text => "Schedule IT | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                                :text => "Schedule IT | About")
+    end
+  end
+end
