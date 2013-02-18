371e5089de620270a1e056c875bdea8c5189d8f0
Mark Russell <sammydavis_jrjr@hotmail.com>
2013-02-18 10:55:37 -0700

    Make changes to scss for layout. Add twitter bootstrap. Change name to Shift Express. Change route file.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,53 +2,46 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home page" do
-
+    before { visit root_path }
     it "should have the content 'Shift Express'" do
-      visit '/'
       page.should have_content('Shift Express')
     end
     it "should have the right title" do
-      visit '/'
       page.should_not have_selector('title',
                                 :text => "Shift Express | Home")
     end
+
   end
 
   describe "Help page" do
-
+    before { visit '/help' }
     it "should have the content 'Help'" do
-      visit '/help'
       page.should have_content('Help')
     end
     it "should have the right title" do
-      visit '/help'
       page.should have_selector('title',
                                 :text => "Shift Express | Help")
     end
   end
 
   describe "About page" do
-
+    before { visit '/about' }
     it "should have the content 'About Us'" do
-      visit '/about'
       page.should have_content('About Us')
     end
     it "should have the right title" do
-      visit '/about'
       page.should have_selector('title',
                                 :text => "Shift Express | About")
     end
   end
 
   describe "Contact page" do
-
+    before { visit '/contact' }
     it "should have the h1 'Contact'" do
-      visit '/contact'
       page.should have_selector('h1', text: 'Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/contact'
       page.should have_selector('title',
                                 text: "Shift Express | Contact")
     end
