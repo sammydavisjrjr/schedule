4de7d9d73fe2924217a3def0978bebe81cfb0e09
Mark Russell <sammydavis_jrjr@hotmail.com>
2013-02-18 13:31:08 -0700

    Add new User controller and View.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Schedule::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
+  match '/signup', to: 'users#new'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
