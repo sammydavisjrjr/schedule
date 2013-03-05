69962f52905812294554dc9be5c61c41e3147be7
Mark Russell <sammydavis_jrjr@hotmail.com>
2013-03-05 10:33:01 -0700

    view users.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -21,19 +21,18 @@ describe "Authentication" do
 
     end
 
-    describe "with valid information" do
+    describe "with valid information"  do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { sign_in user }
 
-      it { should have_selector('title', text: user.name) }
-      it { should have_link('Profile', href: user_path(user)) }
-      it { should have_link('Settings', href: edit_user_path(user)) }
-      it { should have_link('Sign out', href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
+      it { should have_selector('title',    text: user.name) }
+
+      it { should have_link('Users',    href: users_path) }
+
+      it { should have_link('Profile',      href: user_path(user)) }
+      it { should have_link('Settings',     href: edit_user_path(user)) }
+      it { should have_link('Sign out',     href: signout_path) }
+      it { should_not have_link('Sign in',  href: signin_path) }
     end
 
   end
@@ -70,6 +69,12 @@ describe "Authentication" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
         end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
       end
 
       describe "when attempting to visit a protected page" do
