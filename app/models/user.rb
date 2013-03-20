54166b75651cf892f82eb6a736fe896220afab2b
Mark Russell <sammydavis_jrjr@hotmail.com>
2013-03-20 14:31:48 -0700

    add user follow/unfollow microposts.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,23 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
+#  remember_token  :string(255)
+#  admin           :boolean          default(FALSE)
 #
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship",dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   has_secure_password
 
@@ -30,10 +37,27 @@ class User < ActiveRecord::Base
 
 
   def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    microposts
+
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+
+    Micropost.where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+          user_id: self.id)
+
+#    microposts.from_users_followed_by(self)
   end
 
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
 
   private
 
